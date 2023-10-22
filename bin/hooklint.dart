9c6d7988d18894274024dcfc38768bcf0c4aed23
anandzhang <hi@anandzhang.com>
2023-10-22 10:14:02 +0800

    refactor: basic hooklint

--- bin/hooklint.dart
+++ bin/hooklint.dart
@@ -1,43 +1,24 @@
-import 'dart:io';
-import 'dart:isolate';
+import 'package:hooklint/install.dart';
+import 'package:hooklint/run.dart';
 
-import 'package:path/path.dart' as path;
+enum Command {
+  install,
+  run,
+}
 
 void main(List<String> arguments) async {
-  // Windows 未测试
-  if (Platform.isWindows) return;
-
-  // 获取 .git/hooks
-  final ProcessResult result =
-      Process.runSync('git', ['rev-parse', '--git-dir']);
-  if (result.stdout is! String || result.stdout.isEmpty) {
-    print('not a git repository');
+  if (arguments.isEmpty) {
+    print('--help');
     return;
   }
 
-  final File preCommitHook =
-      File('${(result.stdout as String).trim()}/hooks/pre-commit');
-  if (preCommitHook.existsSync()) {
-    // 已有 pre-commit hook 先备份
-    final File backup = File(
-        '${preCommitHook.path}.bak.${DateTime.now().millisecondsSinceEpoch}');
-    backup.writeAsStringSync(preCommitHook.readAsStringSync());
-  }
-
-  // 获取hook模板
-  final Uri? packageUri =
-      await Isolate.resolvePackageUri(Uri.parse('package:hooklint/'));
-  if (packageUri?.path.isEmpty ?? true) {
-    print('not found package');
+  if (arguments.first == Command.install.name) {
+    install();
     return;
   }
-  final File preCommitTemplate = File(
-      path.normalize(path.join(packageUri!.path, '../templates/pre-commit')));
-  // 写入hook
-  preCommitHook.writeAsStringSync(preCommitTemplate.readAsStringSync());
 
-  // 添加执行权限
-  final chmodResult = Process.runSync('chmod', ['+x', preCommitHook.path]);
-  stdout.write(chmodResult.stdout);
-  stderr.write(chmodResult.stderr);
+  if (arguments.first == Command.run.name) {
+    run();
+    return;
+  }
 }
---
+++ lib/run.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+/// run lint
+Future<void> run() async {
+  final ProcessResult result = await Process.run('git', [
+    'diff',
+    '--name-only',
+    '--cached',
+    // Added (A), Copied (C), Modified (M), Renamed (R)
+    // https://git-scm.com/docs/git-diff#Documentation/git-diff.txt---diff-filterACDMRTUXB82308203
+    '--diff-filter=ACMR',
+    '--',
+    '*.dart',
+  ]);
+
+  if (result.exitCode != 0) {
+    stderr.write(result.stderr);
+    return;
+  }
+
+  if (result.stdout is! String) {
+    print('not expected value');
+    return;
+  }
+
+  final stagedResult = (result.stdout as String).trim();
+  if (stagedResult.isEmpty) {
+    print('No staged files found.');
+    return;
+  }
+
+  final List<String> stagedFiles = stagedResult.split('\n');
+
+  // try to fix
+  await Future.wait(stagedFiles.map((file) => Process.start(
+      'dart', ['fix', '--apply', file],
+      mode: ProcessStartMode.inheritStdio)));
+
+  // format
+  final process = await Process.start('dart', ['format', ...stagedFiles],
+      mode: ProcessStartMode.inheritStdio);
+  if (await process.exitCode != 0) return;
+  // re-add
+  await Process.start('git', ['add', ...stagedFiles]);
+
+  // analyze
+  final analyzeProcess = await Process.start(
+      'dart', ['analyze', ...stagedFiles],
+      mode: ProcessStartMode.inheritStdio);
+  final analyzeExitCode = await analyzeProcess.exitCode;
+  if (analyzeExitCode != 0) {
+    print(
+        '\nYou can try to use `hooklint fix` or `dart fix` to fix the above issues.');
+    print('Commit again after no errors.');
+  }
+
+  exit(analyzeExitCode);
+}
